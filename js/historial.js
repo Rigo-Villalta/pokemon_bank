51d50f24ad3463e2d51715fc52af3f4ddd1970c6
Rigoberto Villalta <40904876+Rigo-Villalta@users.noreply.github.com>
2020-12-02 16:37:29 -0600

    Validación sin libreria

--- js/historial.js
+++ js/historial.js
@@ -3,7 +3,7 @@ let ingresos = listaIngresos();
 let ingresosHTML = "";
 
 for (const [key, value] of Object.entries(ingresos)) {
-    ingresosHTML += `<tr><td>${key}</td><td>${value}</td></tr>`;
+    ingresosHTML += `<tr><td>${key}</td><td>$${value}</td></tr>`;
   }
 
 let tablaIngresos = document.getElementById("tabla-ingresos");
@@ -16,7 +16,7 @@ let egresos = listaEgresos();
 let egresosHTML = "";
 
 for (const [key, value] of Object.entries(egresos)) {
-    egresosHTML += `<tr><td>${key}</td><td>${value}</td></tr>`;
+    egresosHTML += `<tr><td>${key}</td><td>$${value}</td></tr>`;
   }
 
 let tablaEgresos = document.getElementById("tabla-egresos");
